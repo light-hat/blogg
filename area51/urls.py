9383ebaed36b1db5bdfe4b70a75815fbdc21e728
Alexey <pirogov30002@gmail.com>
2022-02-22 22:58:30 +0300

    All added settings after deploy

--- area51/urls.py
+++ area51/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('main.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('admin/', admin.site.urls),
+    path('ognajd_admin/', admin.site.urls),
     path('articles/', include('articles.urls'))
 ]
 
--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,4 +1,4 @@
 bind = '127.0.0.1:8000'
-workers = 2
+workers = 3
 user = 'l1ghth4t'
 timeout = 120
